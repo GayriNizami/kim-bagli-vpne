bdf61fc38eef6cb66b01014a6e707b006855773f
Mimar Sinan <9132952+GayriNizami@users.noreply.github.com>
2018-12-01 12:23:40 +0300

    Update kimbagli.sh

--- kimbagli.sh
+++ kimbagli.sh
@@ -1,1 +1,1 @@
-watch -n 1 "strongswan statusall | grep -e identity: -e ESTABLISHED"
+watch -n 1 "sudo strongswan statusall | grep -e identity: -e ESTABLISHED && sudo cat /etc/openvpn/openvpn-status.log | grep 10.8"
